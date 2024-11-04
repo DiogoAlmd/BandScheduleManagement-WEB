f6b68a68cc800b665ce4ac72056c3d797d07a4ea
diogoalmd <diogo.meida@gmail.com>
2024-11-04 19:09:50 -0300

    refactor: unnecessary useEffect

--- src/components/admin/Scale/UpdateScaleModal.tsx
+++ src/components/admin/Scale/UpdateScaleModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -45,17 +45,14 @@ export default function UpdateScaleModal({
   );
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setMusicians(await getMusicians());
-        setInstruments(await getInstruments());
-      } catch {
-        setError("Failed to load musicians or instruments.");
-      }
+  async function fetchData() {
+    try {
+      setMusicians(await getMusicians());
+      setInstruments(await getInstruments());
+    } catch {
+      setError("Failed to load musicians or instruments.");
     }
-    fetchData();
-  }, []);
+  }
 
   const musicianOptions = musicians.map((musician) => ({
     value: musician.id,
@@ -128,7 +125,7 @@ export default function UpdateScaleModal({
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <Button onClick={() => setIsOpen(true)}>Edit Scale</Button>
+        <Button onClick={() => {setIsOpen(true); fetchData();}}>Edit Scale</Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
